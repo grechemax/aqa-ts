6a196b7a2f7da2985ddda700287d23edbf3ade8a
Maks Hrech <grechemax1@gmail.com>
2025-10-27 17:34:43 +0100

    add tests using Mocha and Chai

---
+++ tests/unit/electric-car/tesla.mocha.spec.ts
@@ -0,0 +1,34 @@
+import { assert, expect } from 'chai';
+import { Tesla as Tesla } from '../../../src/lesson9-oop/electric-car/tesla';
+
+describe('Test Tesla', () => {
+    let tesla: Tesla;
+
+    beforeEach(() => {
+        tesla = new Tesla('sedan', 'model S', 0);
+    });
+
+    it('Should be electric', () => {
+        assert.equal(tesla.getFuelType(), 'electric');
+    });
+
+    it('Initial battery level should be 0', () => {
+        expect(tesla.getBatteryLevel()).to.equal(0);
+    });
+
+    it('Should get charged correctly', () => {
+        // 60 mins = 20% battery
+        tesla.charge(60);
+        expect(tesla.getBatteryLevel()).to.equal(20);
+    });
+
+    it('Can not be overcharged', () => {
+        expect(() => tesla.charge(500)).to.throw('Tesla battery cannot be charged more than 300 minutes');
+    });
+
+    it('Should have valid range', () => {
+        tesla.charge(60);
+        const range = tesla.getRange();
+        expect(range).to.be.a('number').and.to.be.greaterThan(0);
+    });
+});
