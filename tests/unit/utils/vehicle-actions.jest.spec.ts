8e11db442bc83b826e21a644d18539f050552dde
Maks Hrech <grechemax1@gmail.com>
2025-10-27 17:35:09 +0100

    add tests Jest

---
+++ tests/unit/utils/vehicle-actions.jest.spec.ts
@@ -0,0 +1,19 @@
+import { driveVehicle } from '../../../src/lesson9-oop/main';
+import { Tesla } from '../../../src/lesson9-oop/electric-car/tesla';
+
+describe('driveVehicle with Tesla', () => {
+    it('should start and accelerate a Tesla', () => {
+        const tesla = new Tesla('SUV', 'Model X', 0);
+        const logSpy = jest.spyOn(console, 'log').mockImplementation();
+
+        driveVehicle(tesla, 70);
+
+        expect(logSpy).toHaveBeenCalledWith('Tesla Model X/SUV started silently \n');
+        expect(logSpy).toHaveBeenCalledWith('Tesla Model X drives at 70 km/h \n');
+
+        tesla.charge(30); // 30 mins = 10% of battery
+        expect(tesla.getRange()).toBe(50); // 10% * 5 = 50 km
+
+        logSpy.mockRestore();
+    });
+});
